3bc5e9def36a324fa60f0fa1c5fd0f06e7623636
mehdi ali <mahediali.k.varaliya456@gmail.com>
2025-04-03 13:09:34 +0530

    Mongo url changes

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ecomApp");
+    await mongoose.connect("mongodb+srv://mehdi:12345@wanderlist.cjdb4.mongodb.net/?retryWrites=true&w=majority&appName=wanderlist");
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed");
