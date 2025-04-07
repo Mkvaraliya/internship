a8369ddd5b6d0d3ad2fab94910000a663dbad0ae
mehdi ali <mahediali.k.varaliya456@gmail.com>
2025-04-07 18:08:17 +0530

    client ui (navbar and product page)

--- src/app/client/Navbar.js
+++ src/app/client/Navbar.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-slate-800 text-5xl'>
-        Navbar
+    <div className='bg-slate-900 text-white flex justify-between items-center p-6 shadow-2xl shadow-gray-700'>
+        <div>
+        <h1 className='text-3xl font-bold'>E-Commerce</h1>
+        </div>
+        <div className='font-semibold text-xl mr-1'>
+          <a className='mr-5 hover:text-gray-300' href='/'>Products</a>
+          <a className='mr-5 hover:text-gray-300' href='/'>About Us</a>
+          <a className='mr-5 hover:text-gray-300' href='/'>Contact Us</a>
+        </div>
+        <div className='flex items-center justify-s'>
+          <button className='bg-red-600  hover:bg-red-400 px-4 py-1 text-xl mx-2 rounded cursor-pointer'>Cart</button>
+          <button className='bg-green-600 hover:bg-green-400 px-4 py-1 text-xl mx-2 rounded cursor-pointer'>Login</button>
+          <button className='bg-indigo-600 hover:bg-indigo-400 px-4 py-1 text-xl mx-2 rounded cursor-pointer'>Register</button>
+        </div>
+
     </div>
   )
 }
--- src/app/client/page.js
+++ src/app/client/page.js
@@ -1,8 +1,77 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const page = () => {
+  const [product, setProduct] = useState([]);
+
+  const getData = async() => {
+    const request = await axios.get("/api/products");
+    const response = request.data;
+    setProduct(response.products);
+  }
+
+  useEffect(() => {
+    getData();
+  },[]);
+
   return (
-    <div>
-       i am client
+    <div className="w-80% bg-slate-400 ">
+      {/* Our Cart Section */}
+      <div  className="w-full flex flex-wrap gap-3 items-center px-10 mb-5">
+        {product.map((products) => (  
+           <div key={products._id} className="w-70 flex flex-col flex-wrap mt-5 bg-white items-center rounded h-85  p-3 hover:bg-gray-300">
+           <img className="w-50 h-40" src={products.image}></img>
+           <p className="mt-2 text-xl font-bold">{products.title}</p>
+           <p className="text-gray-600 font-semibold">{products.description}</p>
+           <p className="text-red-500 mt-1 font-bold text-xl">{products.price}</p>
+           <button className="bg-amber-400 mt-1 py-2 px-3 rounded">Add to Cart</button>
+         </div>
+        ))}
+       
+
+      </div>
+
+    {/* Our Footer Section  */}
+     <div className="bg-gray-900 text-white items-center md:place-items-start px-4 pt-8 w-full flex flex-col  md:flex-row flex-wrap justify-between">
+        <div className="mb-3">
+            <img className="w-40 h-16" src="https://pwskills.com/images/PWSkills-white.png"></img>
+            <p className="my-4">Email us: support@gmail.com</p>
+            <img className="w-40 h-16" src="https://tse2.mm.bing.net/th?id=OIP.hk0NCRHrF6pwTzBjwTeBOAHaCl&pid=Api&P=0&h=220"></img>
+            
+        </div>
+
+        <div className="mb-3 w-[209px]">
+            <h2 className="font-bold text-xl mt-4">PW SKills</h2>
+            <div className="w-32 h-1 border-2 border-yellow-400 rounded-2xl my-2"></div>
+            <div>
+                <p>About Us</p>
+                <p>FAQs</p>
+                <p>Privacy Policy</p>
+            </div>
+        </div>
+        <div className="mb-3 w-[209px]">
+            <h2 className="font-bold text-xl mt-4">Products</h2>
+            <div className="w-32 h-1 border-2 border-yellow-400 rounded-2xl my-2"></div>
+            <div>
+                <p>PW Skills Lab</p>
+                <p>Job Portal</p>
+                <p>Experience Portal</p>
+                <p>Become an affiliate</p>
+                <p>Hall of Fame</p>
+            </div>
+        </div>
+        <div className="mb-3 w-[209px]">
+            <h2 className="font-bold text-xl mt-4">Links</h2>
+            <div className="w-32 h-1 border-2 border-yellow-400 rounded-2xl my-2"></div>
+            <div>
+                <p>Discord Channel</p>
+                <p>PW Youtube</p>
+                <p>Careers</p>
+            </div>
+        </div>
+    </div>
+     
     </div>
   )
 }
